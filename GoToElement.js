f6ce9e0a2bdfa8115e469fb4f5326f5851464fba
lay0020 <lay0020@gmail.com>
2020-07-12 14:20:40 +0800

    取得所有元素

--- GoToElement.js
+++ GoToElement.js
@@ -1,23 +1,37 @@
 window.onload = () => {
     //console.log("頁面載入完成~");
 
-    // 取得元素
-    var test = document.getElementById("test");
+    // 取得單一元素
+    // var test = document.getElementById("test");
 
-    // 元素.點擊事件
-    test.onclick = () => {
-        var target = test.getAttribute("data-gte-target");   //目標屬性
-        var offset = test.getAttribute("data-gte-offset");   //位移屬性
+    // 取得所有元素 -"*" 表抓取所有標籤
+    var element = document.getElementsByTagName("*");
 
-        var element = document.getElementById(target);       //目標元素
+    //利用迴圈來抓取
+    for (let index = 0; index < element.length; index++) {
+        const e = element[index];
 
-        // console.log(target);
-        // console.log(offset);
+        // 元素.點擊事件
+        e.onclick = () => {
+            var target = e.getAttribute("data-gte-target");   //目標屬性
+            var offset = e.getAttribute("data-gte-offset");   //位移屬性
 
-        //視窗.捲動至({上方,行為})
-        window.scrollTo({
-            top: element.offsetTop,
-            behavior: "smooth"
-        })
+            // 不要抓取h1、section等標籤
+            if (target){
+
+                var element = document.getElementById(target);    //目標元素
+    
+                // console.log(target);
+                // console.log(offset);
+    
+                //視窗.捲動至({上方,行為})
+                window.scrollTo({
+                    top: element.offsetTop,
+                    behavior: "smooth"
+                })
+            }
+
+        }
+
     }
 }
